be36c14e4aab50edd018056e7ceec15e85448719
Liza <lfortuin65@gmail.com>
2023-01-22 21:46:44 +0200

    added more sphinx implementation

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import django
-sys.path.insert(0, os.path.abspath(0, os.path.abspath('..')))
+sys.path.insert(0, os.path.abspath('..'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'my_site.settings'
 # Configuration file for the Sphinx documentation builder.
 #
@@ -32,5 +32,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
